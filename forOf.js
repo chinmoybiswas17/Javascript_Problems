2f16f3dbf033087471f1b437aad1ad17bb20196f
Your Name <you@example.com>
2025-05-18 16:50:23 +0600

    first commit

---
+++ forOf.js
@@ -0,0 +1,18 @@
+const number=[1,2,3,4,5]
+ for(sum of number){
+    console.log(sum)
+ }
+
+ const obj = {
+    name: "Chinmoy",
+    age: 26,
+    Proffetion: "AD, Bangladesh Bank"
+  };
+  
+  for (let key in obj) {
+    console.log(obj[key]);
+  }
+  for (let key in obj) {
+    console.log(`${key}: ${obj[key]}`);
+  }
+  
